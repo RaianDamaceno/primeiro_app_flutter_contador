708671b4ec18ced102fa272b12448cb04ab2b931
Raian Damaceno <36999338+RaianDamaceno@users.noreply.github.com>
2021-01-12 11:10:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# primeiro_app_flutter_contador
+ 
---
+++ android/app/src/main/kotlin/com/example/primeiro_app_flutter_contador/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.primeiro_app_flutter_contador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
